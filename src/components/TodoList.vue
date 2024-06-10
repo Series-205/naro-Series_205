347fe316cc40cf8938a4ce925f11177fe8eb8d77
Series-205 <esio.trot28@gmail.com>
2024-06-10 22:36:09 +0900

    add: TodoList 完了済と未完のタスクを分けて表示

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import ItemList from './ItemList.vue'
+
+interface Task {
+  name: string
+  isFinished: boolean
+}
+
+const tasks = ref<Task[]>([
+  { name: '教科書購入', isFinished: false },
+  { name: 'レポート', isFinished: false },
+  { name: '環境構築', isFinished: true }
+])
+</script>
 
 <template>
   <div>TodoList</div>
+  <div>完了済</div>
+  <ul>
+    <template v-for="task in tasks" :key="task.name">
+      <li v-if="task.isFinished">
+        {{ task.name }}
+      </li>
+    </template>
+  </ul>
+  <div>未完</div>
+  <ul>
+    <template v-for="task in tasks" :key="task.name">
+      <li v-if="!task.isFinished">
+        {{ task.name }}
+      </li>
+    </template>
+  </ul>
 </template>
 
 <style></style>
