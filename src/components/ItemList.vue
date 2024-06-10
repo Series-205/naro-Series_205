b82ad1a2a65cd75d2ff570535bbc7cff3c305ad8
Series-205 <esio.trot28@gmail.com>
2024-06-10 21:29:08 +0900

    add: ItemList のリスト表示部分

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Item {
+  name: string
+  price: number
+}
+
+const items = ref<Item[]>([
+  { name: 'たまご', price: 100 },
+  { name: 'りんご', price: 160 }
+])
+</script>
 
 <template>
   <div>ItemList</div>
+  <ul>
+    <li v-for="item in items" :key="item.name">
+      <div>名前: {{ item.name }}</div>
+      <div>{{ item.price }} 円</div>
+    </li>
+  </ul>
 </template>
 
 <style></style>
