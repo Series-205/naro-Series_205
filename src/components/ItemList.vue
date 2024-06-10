2c6bce5e370e14a0ec7b60a66b62a4947cdccd25
Series-205 <esio.trot28@gmail.com>
2024-06-10 21:54:12 +0900

    add: ItemList price >= 500 なら赤くする

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -21,7 +21,7 @@ const addItem = () => {
 <template>
   <div>ItemList</div>
   <ul>
-    <li v-for="item in items" :key="item.name">
+    <li v-for="item in items" :key="item.name" :class="{ over500: item.price >= 500 }">
       <div>名前: {{ item.name }}</div>
       <div>{{ item.price }} 円</div>
     </li>
@@ -38,4 +38,8 @@ const addItem = () => {
   </div>
 </template>
 
-<style></style>
+<style>
+.over500 {
+  color: red;
+}
+</style>
