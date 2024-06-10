08f66fa1ae64c8fc35980b97905e816c49796730
Series-205 <esio.trot28@gmail.com>
2024-06-10 21:36:47 +0900

    add: ItemList に追加ボタン

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { isTemplateExpression } from 'typescript'
 import { ref } from 'vue'
 
 interface Item {
@@ -10,6 +11,12 @@ const items = ref<Item[]>([
   { name: 'たまご', price: 100 },
   { name: 'りんご', price: 160 }
 ])
+const newItemName = ref('')
+const newItemPrice = ref(0)
+
+const addItem = () => {
+  items.value.push({ name: newItemName.value, price: newItemPrice.value })
+}
 </script>
 
 <template>
@@ -20,6 +27,16 @@ const items = ref<Item[]>([
       <div>{{ item.price }} 円</div>
     </li>
   </ul>
+  <div>
+    <label>
+      名前
+      <input v-model="newItemName" type="text" />
+    </label>
+    <label>
+      <input v-model="newItemPrice" type="number" />
+    </label>
+    <button @click="addItem">追加</button>
+  </div>
 </template>
 
 <style></style>
